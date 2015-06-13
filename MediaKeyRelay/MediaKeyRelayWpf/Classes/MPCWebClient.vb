927177a57b882e0952a6edc4a7f59b819f24ef17
valentinb102 <vben@live.com>
2015-06-13 00:06:30 -0400

    sockets are hard to do adding webclient calls to mpc-hc

---
+++ MediaKeyRelay/MediaKeyRelayWpf/Classes/MPCWebClient.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Net
+
+Public Class MPCWebClient
+
+    Public webclient As New WebClient
+    Private address As String
+
+    Public Enum Commands
+        PlayPause = 889
+        [Stop] = 890
+        [Next] = 922
+        Previous = 921
+    End Enum
+
+    Public Sub New(address)
+        Me.address = address
+    End Sub
+
+    Public Function PlayPause() As System.Threading.Tasks.Task(Of String)
+        webclient.QueryString.Add("wm_command", Commands.PlayPause)
+        Return webclient.DownloadStringTaskAsync(New Uri(address))
+    End Function
+
+End Class
--- MediaKeyRelay/MediaKeyRelayWpf/MainWindow.xaml.vb
+++ MediaKeyRelay/MediaKeyRelayWpf/MainWindow.xaml.vb
@@ -1,4 +1,7 @@
-﻿Class MainWindow
+﻿Imports System.Net
+Imports System.Threading.Tasks
+
+Class MainWindow
     Private hook As KeyboardHook
 
     Private Sub btnKBHook_Click(sender As Object, e As RoutedEventArgs)
@@ -29,5 +32,21 @@
 
     Private Sub btnNext_Click(sender As Object, e As RoutedEventArgs)
         KeyEvent.FireKeyCode(KeyEvent.KeyCodes.VK_MEDIA_NEXT_TRACK)
+    End Sub
+
+    Private Sub btnMPCStartConn_Click(sender As Object, e As RoutedEventArgs)
+        Dim mpcclient As New MPCWebClient(txtMPCURL.Text)
+
+
+
+        txtMPCStatus.Text &= "play pause started" & Environment.NewLine
+
+        mpcclient.PlayPause().ContinueWith(Sub()
+                                               datacomplete()
+                                           End Sub, TaskScheduler.FromCurrentSynchronizationContext)
+    End Sub
+
+    Private Sub datacomplete()
+        txtMPCStatus.Text &= "play pause finished" & Environment.NewLine
     End Sub
 End Class
