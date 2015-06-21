4f782610320fc9a2192eae1416e3a81abe0c2321
valentinb102 <vben@live.com>
2015-06-21 15:07:33 -0400

    adding add/remove startup registry entry

---
+++ MediaKeyRelay/MediaKeyRelayWpf/Classes/Startup/StartupHelper.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.Win32
+
+Public Class StartupHelper
+
+    Public Shared Sub AddApplicationToCurrentUserStartup()
+        Using key As RegistryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+            key.SetValue("My ApplicationStartUpDemo", """" & Reflection.Assembly.GetExecutingAssembly().Location & """")
+        End Using
+    End Sub
+
+    Public Shared Function ApplicationExistsInCurrentUserStartup() As Boolean
+
+        Return True
+    End Function
+End Class
